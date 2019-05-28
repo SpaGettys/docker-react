feca6e159709e2076585e2a4908ba96ca24f74e8
SpaGettys <jgettys@gmail.com>
2019-05-28 21:17:17 +0900

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sweet <code>src/App.js</code> and save to reload.
+          I was changed on the Feature branch! <code>src/App.js</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
